f0c59567b2a73554fda40ce1f169e9ca6e18eb80
Tarek <tareqassiri2000@gmail.com>
2023-05-28 23:35:49 +0300

    fixed an issue with read , and made unzip quiet.

--- spring.sh
+++ spring.sh
@@ -5,18 +5,23 @@ language=$(echo "$json" | jq -r '.language.values[] | "\(.name)\t\(.id)"' | fzf 
 packaging=$(echo "$json" | jq -r '.packaging.values[] | "\(.name)\t\(.id)"' | fzf  --delimiter='\t' --with-nth=1 | cut -f2 | awk -v d="," '{s=(NR==1?s:s d)$0}END{print s}')
 javaVersion=$(echo "$json" | jq -r '.javaVersion.values[] | "\(.name)\t\(.id)"' | fzf  --delimiter='\t' --with-nth=1 | cut -f2 | awk -v d="," '{s=(NR==1?s:s d)$0}END{print s}')
 bootVersion=3.1.0
-read -r -p "group (ex. com.example): " groupId
-read -r -p "artifact (ex. demo): " artifactId
-read -r -p "name (ex. demo): " name
-read -r -p "description (ex. Demo project for Spring Boot): " description
+echo -n "group (ex. com.example): "
+read -r groupId
+echo -n "artifact (ex. demo): "
+read -r artifactId
+echo -n "name (ex. demo): "
+read -r name
+echo -n "description (ex. Demo project for Spring Boot): "
+read -r description
 description=${description// /%20}
-read -r -p "packageName (ex. com.example.demo): " packageName
+echo -n "packageName (ex. com.example.demo): "
+read -r packageName
 baseDir=$name
 dependencies=$(echo "$json"| jq -r '.dependencies.values[].values[] | "\(.name)\t\(.id)"' | fzf -m --delimiter='\t' --with-nth=1 | cut -f2 | awk -v d="," '{s=(NR==1?s:s d)$0}END{print s}')
 URL="https://start.spring.io/starter.zip?type=$type&language=$language&bootVersion=$bootVersion&baseDir=$baseDir&groupId=$groupId&artifactId=$artifactId&name=$name&description=$description&packageName=$packageName&packaging=$packaging&javaVersion=$javaVersion&dependencies=$dependencies"
 
-echo "$URL"
-curl "$URL" -o do.zip
+# echo "$URL"
+curl -s "$URL" -o do.zip
 
-unzip do.zip
+unzip -qq do.zip
 rm do.zip
